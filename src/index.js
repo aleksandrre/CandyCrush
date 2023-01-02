5aeb68b71af48208406d59500e85731f11c033f0
aleksandrre <alekochokheli.01@gmail.com>
2023-01-02 16:52:24 +0400

    make p.tag

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App'
-import CreateBoard from './App'
-import Counter from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import CreateBoard from "./App";
+import Counter from "./App";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
-    
-    <Counter/>
-   
-    
+    <Counter />
+
+    <h1>see</h1>
   </div>
-  
 );
-
-
